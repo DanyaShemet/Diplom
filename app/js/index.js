96af2ae61d01afbf3c5ba09f942df77ba37ba4d9
Danya <shemetg@gmail.com>
2019-05-28 22:31:26 +0300

    Add files via upload

--- app/js/index.js
+++ app/js/index.js
@@ -2,13 +2,30 @@ let mobile = document.querySelectorAll('.game');
 
 let btnPc = document.querySelector('.only-pc');
 let btnAll = document.querySelector('.all');
+let bttMb = document.querySelector('.only-mob');
 
 btnPc.onclick = function(){
     mobile.forEach((elem) =>{
         if(elem.getAttribute('data-name') != 'pc'){
             elem.style.display = 'none'
             btnAll.classList.remove('active');
+            bttMb.classList.remove('active');
             btnPc.classList.add('active');
+        }else{
+            elem.style.display = 'block'
+        }
+        
+    })
+}
+bttMb.onclick = function(){
+    mobile.forEach((elem) =>{
+        if(elem.getAttribute('data-name') != 'mb'){
+            elem.style.display = 'none'
+            btnAll.classList.remove('active');
+            btnPc.classList.remove('active');
+            bttMb.classList.add('active');
+        }else{
+            elem.style.display = 'block'
         }
         
     })
@@ -18,6 +35,7 @@ btnAll.onclick = function(){
         
             elem.style.display = 'block'
             btnPc.classList.remove('active');
+            bttMb.classList.remove('active');
             btnAll.classList.add('active');
         
         
